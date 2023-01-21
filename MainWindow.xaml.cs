9af005b24f6e838e38edce19129c3db8b586346c
Filipe Eduardo Andrade Costa <filipeeacosta@gmail.com>
2023-01-21 14:13:46 -0300

    Click events

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.Windows;
 
 namespace PDF_Converter
 {
@@ -14,7 +16,42 @@ namespace PDF_Converter
 
         private void ConvertButton_Click(object sender, RoutedEventArgs e)
         {
+            if (pathTextBox.Text == string.Empty)
+            {
+                MessageBox.Show("Please select a file", "Attention", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
 
+            switch (conversionDropDown.SelectedIndex)
+            {
+                case 0: //Convert Doc to PDF
+
+                    break;
+                default:
+                    MessageBox.Show("Please select an option", "Attention", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    return;
+            }
+        }
+
+        private void SelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            bool? result = openFileDialog.ShowDialog();
+
+            if (result.HasValue && result.Value)
+            {
+                pathTextBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void OpenFolder(string folderPath)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo()
+            {
+                Arguments = folderPath.Substring(0, folderPath.LastIndexOf('\\')),
+                FileName = "explorer.exe"
+            };
+            Process.Start(startInfo);
         }
     }
 }
